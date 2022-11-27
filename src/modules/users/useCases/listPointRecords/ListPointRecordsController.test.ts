909f145432310196aefb97e3e6be6090e181e60f
Marcos André <72817154+Marcos-afk@users.noreply.github.com>
2022-11-26 21:11:02 -0300

    test: implementation of integration tests in the point record listing use case

---
+++ src/modules/users/useCases/listPointRecords/ListPointRecordsController.test.ts
@@ -0,0 +1,38 @@
+import request from 'supertest';
+import { app } from '@shared/infra/http/app';
+import { AppSource, InitializeConnection } from '@shared/infra/typeorm';
+import { hash } from 'bcryptjs';
+
+describe('List point records controller', () => {
+  beforeAll(async () => {
+    await InitializeConnection();
+    await AppSource.runMigrations();
+    const password = await hash('admin', 8);
+
+    AppSource.query(`INSERT INTO users(name, email, password, role)
+    values('admin', 'admin@point.com.br', '${password}', 'administrador')`);
+  });
+
+  afterAll(async () => {
+    await AppSource.dropDatabase();
+    await AppSource.destroy();
+  });
+
+  it('should be able to record user point', async () => {
+    const responseToken = await request(app).post('/api/v1/users/login').send({
+      email: 'admin@point.com.br',
+      password: 'admin',
+    });
+
+    const { token } = responseToken.body.response;
+
+    const response = await request(app)
+      .get('/api/v1/point-records/list')
+      .set({
+        Authorization: `Bearer ${token}`,
+      });
+
+    expect(response.body.message).toBe('Lista de registro de pontos!');
+    expect(response.statusCode).toBe(200);
+  });
+});
